91a00f116e49a7e98e73739271bf22064d86de24
Daniil211 <id20214@gmail.com>
2023-05-12 16:03:52 +0300

    Добавьте файлы проекта.

---
+++ TestTask/GetWeatherData.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestTask
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class GetWeatherData : Window, INotifyPropertyChanged
+    {
+        private readonly WeatherService _weatherService = new WeatherService();
+        private WeatherData _weatherData;
+
+        public GetWeatherData()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private bool _isWeatherDataLoaded;
+
+        public string Temperature => _isWeatherDataLoaded ? _weatherData?.Main?.Temperature.ToString("0.#") + "°C" : string.Empty;
+        public string Description => _isWeatherDataLoaded ? _weatherData?.Weather?[0]?.Description : string.Empty;
+        public string WindSpeed => _isWeatherDataLoaded ? _weatherData?.Wind?.Speed.ToString("0.#") + " m/s" : string.Empty;
+
+        private async void OnGetWeatherClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _weatherData = await _weatherService.GetWeatherDataAsync(cityTextBox.Text);
+                _isWeatherDataLoaded = true;
+                OnPropertyChanged(nameof(Temperature));
+                OnPropertyChanged(nameof(Description));
+                OnPropertyChanged(nameof(WindSpeed));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to get weather data: {ex.Message}");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
---
+++ TestTask/WeatherService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class WeatherService
+{
+    private const string ApiKey = "957b7a4402647decff3b8162a591a350";
+    private const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+    public async Task<WeatherData> GetWeatherDataAsync(string city)
+    {
+        using (var client = new HttpClient())
+        {
+            var url = $"{BaseUrl}?q={city}&appid={ApiKey}&units=metric";
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<WeatherData>(json);
+                return data;
+            }
+            else
+            {
+                throw new Exception($"Failed to get weather data. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
+
+public class WeatherData
+{
+    [JsonProperty("main")]
+    public TemperatureData Main { get; set; }
+
+    [JsonProperty("weather")]
+    public DescriptionData[] Weather { get; set; }
+
+    [JsonProperty("wind")]
+    public SpeedData Wind { get; set; }
+}
+
+public class TemperatureData
+{
+    [JsonProperty("temp")]
+    public double Temperature { get; set; }
+
+    [JsonProperty("feels_like")]
+    public double FeelsLike { get; set; }
+}
+
+public class DescriptionData
+{
+    [JsonProperty("description")]
+    public string Description { get; set; }
+}
+
+public class SpeedData
+{
+    [JsonProperty("speed")]
+    public double Speed { get; set; }
+}
